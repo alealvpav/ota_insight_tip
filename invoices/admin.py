50785bec5dcd5d46c41782abaa6d53e404d4332b
Alejandro Álvarez <alealvpav@gmail.com>
2022-07-11 22:54:59 +0200

    Prevent invoices to be deleted by users in admin

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Invoice
 
+
+class InvoiceAdmin(admin.ModelAdmin):
+    exclude = ('visible',)
+
+    def has_delete_permission(self, request, obj=None):
+        """
+        Override to prevent admin panel users to delete invoices. This will
+        make the delete button not to appear ever.
+        :return: False
+        :rtype: bool
+        """
+        return False
+
+
 # Register your models here.
-admin.site.register(Invoice)
+admin.site.register(Invoice, InvoiceAdmin)
